5665fe179b3abe4f4a8a9dd90011a9ed8ceadc03
Yurii Urshanskyi <59485534+yuriiurshanskyi@users.noreply.github.com>
2021-01-05 13:11:36 +0200

    Add files via upload

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,46 @@
+﻿using Gazda.Models;
+using Gazda.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Gazda.Controllers
+{
+    [Route("/api/Comment")]
+    [ApiController]
+    public class CommentController : Controller
+    {
+        public CommentController(JsonFileCommentService jsonFileCommentService)
+        {
+            JsonFileCommentService = jsonFileCommentService;
+        }
+        public JsonFileCommentService JsonFileCommentService { get;}
+
+        [HttpPost]
+        public IActionResult Post([FromForm]Comment comment)
+        {
+
+            comment.Id = Guid.NewGuid().ToString();
+
+            JsonFileCommentService.AddComment(comment);
+
+            return Ok(comment);
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var comments = JsonFileCommentService.GetComments();
+            return Json(comments);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] string id)
+        {
+            JsonFileCommentService.ChangeLike(id);
+            return Ok("Successfull PUT");
+        }
+
+    }
+}
