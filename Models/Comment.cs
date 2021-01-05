f85f2bd195a962fa12c787158545a645b864e818
Yurii Urshanskyi <59485534+yuriiurshanskyi@users.noreply.github.com>
2021-01-05 13:11:44 +0200

    Add files via upload

---
+++ Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gazda.Models
+{
+    public class Comment
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public bool isLiked { get; set; }
+
+    }
+}
